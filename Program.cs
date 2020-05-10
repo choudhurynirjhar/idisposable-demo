69318739b7227e1fd505fe023a49be25fb9d75ff
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2020-05-10 16:39:14 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IDisposable.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using var serviceProxy = new ServiceProxy(null);
+
+            serviceProxy.Get();
+            // DO something 
+            serviceProxy.Post("");
+            // Other code
+        }
+    }
+}
---
+++ ServiceProxy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+
+namespace IDisposable.Demo
+{
+    public class ServiceProxy : System.IDisposable
+    {
+        private readonly HttpClient httpClient;
+        private bool disposed;
+
+        public ServiceProxy(IHttpClientFactory httpClientFactory)
+        {
+            httpClient = httpClientFactory.CreateClient();
+        }
+
+        ~ServiceProxy()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Dispose managed objects
+                httpClient.Dispose();
+            }
+            // Dispose unmanaged objects
+
+            disposed = true;
+        }
+
+        public void Get()
+        {
+            var response = httpClient.GetAsync("");
+        }
+
+        public void Post(string request)
+        {
+            var response = httpClient.PostAsync("", new StringContent(request));
+        }
+    }
+}
